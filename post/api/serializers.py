df45751cfbfdd925e9373d3c1e3199773bdf8bd6
Your Name <you@example.com>
2022-04-21 16:34:51 +0300

    add image, id, url

--- post/api/serializers.py
+++ post/api/serializers.py
@@ -3,14 +3,16 @@ from post.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField()
+    url = serializers.HyperlinkedIdentityField(view_name='detail', read_only=True)
 
     class Meta:
         model = Post
-        fields = ['user', 'title', 'content']#create fields that will be in api
+        fields = ['id', 'url', 'user', 'title', 'content', 'image']#create fields that will be in api
 
 
-class PostCreateSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ['user', 'title', 'content']
+# class PostCreateSerializer(serializers.ModelSerializer):
+#
+#     class Meta:
+#         model = Post
+#         fields = ['user', 'title', 'content', 'image']
--- post/models.py
+++ post/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField(null=True, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='images', null=True)
+
 
     def __str__(self):
         return self.title
